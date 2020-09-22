93d338f2aee4812ce9bde09bf0d2c154bf5cb671
sunny babal <sbabal@hotmail.com>
2020-09-21 22:02:19 -0500

    updated the server js file

--- server.js
+++ server.js
@@ -1,10 +1,32 @@
 var express = require("express");
 var path = require("path");
+const { getMaxListeners } = require("process");
 var app = express();
 var PORT = process.env.PORT || 3000;
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+var reservations = [
+    {
+      //slug: "johndoe",
+      customerName: "John Doe",
+      phoneNumber: "5555555555",
+      customerEmail: "hello@gmail.com",
+      customerID: 2000
+    }
+  ];
+
+  var waitList = [
+    {
+      customerName: "John Doe",
+      phoneNumber: "5555555555",
+      customerEmail: "hello@gmail.com",
+      customerID: 2000
+    }
+  ];
+
+
 
 app.get("/",function(req, res){
     res.sendFile(path.join(__dirname, "index.html"))
@@ -15,58 +37,33 @@ app.get("/add", function(req, res) {
 app.get("/view", function(req, res) {
     res.sendFile(path.join(__dirname, "view.html"));
   });
-
-app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+  app.get("/api/tables", function(req, res) {
+    return res.json(reservations);
   });
+  app.get("/api/waitlist", function(req, res) {
+    return res.json(waitList)
+});
+
 
   app.post("/api/tables", function(req, res) {
     // req.body hosts is equal to the JSON post sent from the user
     // This works because of our body parsing middleware
     var newReservation = req.body;
-  
-    // Using a RegEx Pattern to remove spaces from newCharacter
+    // Using a RegEx Pattern to remove spaces from newReservation
     // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-    newReservation.routeName = newReservation.name.replace(/\s+/g, "").toLowerCase();
-  
+   //-- newReservation.slug = newReservation.name.replace(/\s+/g, "").toLowerCase();
     console.log(newReservation);
-  
-    characters.push(newReservation);
-  
-    res.json(newReservation);
+    reservations.push(newReservation);
+    if(reservations.length<=4){
+     reservations.push(newReservation)
+    }
+    else{
+   waitList.push(newReservation);
+}
+     res.json(newReservation)
   });
 
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 
-
-//-----------------------------------------------------------------------------------------------
-//   for (var i = 0; i < characters.length; i++) {
-//     if (chosen === characters[i].routeName) {
-//       return res.json(characters[i]);
-//     }
-//   }
-
-//   return res.json(false);
-// });
-
-// // Create New Characters - takes in JSON input
-app.post("/api/characters", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body parsing middleware
-  var newCharacter = req.body;
-
-  // Using a RegEx Pattern to remove spaces from newCharacter
-  // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
-
-  console.log(newCharacter);
-
-  characters.push(newCharacter);
-
-  res.json(newCharacter);
-});
-
-// // Starts the server to begin listening
-// // =============================================================
-// app.listen(PORT, function() {
-//   console.log("App listening on PORT " + PORT);
-// });
